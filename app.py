fbf90235adc8ed423fd3510788538b349cbd5066
hgrunt54 <hgrunt54@gmail.com>
2020-02-03 20:23:35 -0600

    Options from SQLite Database

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-"""
-This script runs the application using a development server.
-It contains the definition of routes and views for the application.
-"""
-
+import sqlite3
+import flask
+from sqlite3 import Error
+from contextlib import closing
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -14,12 +13,43 @@ wsgi_app = app.wsgi_app
 def index():
     background = 'White'
     font = 'Black'
-    bgColors = ['Red', 'Blue', 'Green']
-    fColors = ['Yellow', 'Purple', 'Black']
+    bgColors = getColors()
+    fColors = getColors()
     if request.method == 'POST':
         background = request.form['background']
         font = request.form['font']
     return render_template('index.html', bgColors=bgColors, fColors=fColors, background=background, font=font)
+
+def connectdb():
+    conn = sqlite3.connect('Colors.db', check_same_thread=False)
+    return conn
+
+def countColors():
+    conn = connectdb()
+    c = conn.cursor()
+    colorCount = '''SELECT * FROM Colors'''
+    c.execute(colorCount)
+    colorCounter = len(c.fetchall())
+    c.close()
+    return colorCounter
+
+def getColors():
+    colors = []
+    x = 0
+    i = countColors()
+    conn = connectdb()
+    c = conn.cursor()
+    query = '''SELECT Color FROM Colors'''
+    c.execute(query)
+    while x < i:
+        color = c.fetchone()
+        colors.append(color[0])
+        x += 1
+    c.close()
+    return colors
+
+
+
 
 if __name__ == '__main__':
     import os
@@ -29,3 +59,4 @@ if __name__ == '__main__':
     except ValueError:
         PORT = 5555
     app.run(HOST, PORT)
+    
